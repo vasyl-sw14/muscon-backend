813e59d451468a7728e1ace5fd941d56fc9096d6
vasyl-sw14 <vasyl.shponarskyi@gmail.com>
2021-12-29 15:20:48 +0200

    added genres and artist endpoints

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, g
+from flask import Flask, g, jsonify, request
 import sqlite3
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 DATABASE = "./test.db"
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="7ed78a10ad33404c952dec000863ec9e",
+                                                           client_secret="df0f57a438454795a9b1da87825000de"))
 
 
 def get_db():
@@ -16,8 +21,32 @@ def get_db():
 
 @app.route('/', methods=['GET'])
 def home():
-    print(get_db())
     return "MusCon Backend"
 
 
-app.run()
+@app.route('/genres', methods=['GET'])
+def get_genres():
+    result = sp.recommendation_genre_seeds()
+    print(result)
+    return jsonify(result['genres'])
+
+
+@app.route('/get_artists', methods=['POST'])
+def get_artists():
+    genres = request.form.get('genres')
+    if (genres is None):
+        return "Please enter valid data", 400
+    artists = []
+    result = sp.recommendations(seed_genres=genres.split(', '), limit=20)
+    for track in result['tracks']:
+        for artist in track['artists']:
+            artist_data = sp.artist(artist['uri'])
+            if not any(a['name'] == artist_data['name'] for a in artists) and len(artist_data['images']) > 0:
+                artists.append(
+                    {'name': artist_data['name'], 'image': artist_data['images'][0], 'popularity': artist_data['popularity']})
+    artists.sort(key=lambda x: x['popularity'], reverse=True)
+    return jsonify(artists)
+
+
+if __name__ == "__main__":
+    app.run()
